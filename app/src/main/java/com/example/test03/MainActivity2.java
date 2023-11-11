82a063a52f852a470cc7af2c91624ffb4023dea5
Reza Mortazavi <ir1979@gmail.com>
2023-11-11 16:58:50 +0330

    ActivityMain2 contains 0 to 3 buttons using getResources() function.

--- app/src/main/java/com/example/test03/MainActivity2.java
+++ app/src/main/java/com/example/test03/MainActivity2.java
@@ -2,6 +2,7 @@ package com.example.test03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,35 +20,60 @@ public class MainActivity2 extends AppCompatActivity {
         EditText edtCalc = findViewById(R.id.edtCalculation);
         TextView txtResult = findViewById(R.id.txtResult2);
 
-        Button btn_0, btn_1, btn_2, btn_3;
-        btn_0 = findViewById(R.id.btn_0);
-        btn_1 = findViewById(R.id.btn_1);
-        btn_2 = findViewById(R.id.btn_2);
-        btn_3 = findViewById(R.id.btn_3);
+//        Button btn_0, btn_1, btn_2, btn_3;
+//        btn_0 = findViewById(R.id.btn_0);
+//        btn_1 = findViewById(R.id.btn_1);
+//        btn_2 = findViewById(R.id.btn_2);
+//        btn_3 = findViewById(R.id.btn_3);
+
+//        btn_0.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (edtCalc.getText().toString().equals("0"))
+//                    edtCalc.setText("0");
+//                else
+//                    edtCalc.setText(edtCalc.getText().toString() + "0");
+//            }
+//        });
+//
+//
+//        btn_1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (edtCalc.getText().toString().equals("0"))
+//                    edtCalc.setText("1");
+//                else
+//                    edtCalc.setText(edtCalc.getText().toString() + "1");
+//            }
+//        });
 
 
-        btn_0.setOnClickListener(new View.OnClickListener() {
+        Button currentButton;
+
+        Resources rr = getResources();
+
+        for (int i=0; i<4; i++)
+        {
+            String currentName = "btn_" + Integer.toString(i);
+            int currentId = rr.getIdentifier(currentName, "id", getPackageName());
+
+            currentButton = findViewById(currentId);
+
+            final int final_i = i;
+        currentButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (edtCalc.getText().toString().equals("0"))
-                    edtCalc.setText("0");
+                    edtCalc.setText(Integer.toString(final_i));
                 else
-                    edtCalc.setText(edtCalc.getText().toString() + "0");
+                    edtCalc.setText(edtCalc.getText().toString() + Integer.toString(final_i));
             }
         });
 
 
-        btn_1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (edtCalc.getText().toString().equals("0"))
-                    edtCalc.setText("1");
-                else
-                    edtCalc.setText(edtCalc.getText().toString() + "1");
-            }
-        });
 
 
+        }
 
 
 

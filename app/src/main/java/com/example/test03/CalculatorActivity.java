3508237213dca44051cbb014fc8bf77b4e5c1623
Reza Mortazavi <ir1979@gmail.com>
2023-11-25 11:08:18 +0330

    no change

--- app/src/main/java/com/example/test03/CalculatorActivity.java
+++ app/src/main/java/com/example/test03/CalculatorActivity.java
@@ -3,6 +3,7 @@ package com.example.test03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -35,6 +36,7 @@ public class CalculatorActivity extends AppCompatActivity {
                 txtCalc.setText(txtCalc.getText().toString() + "0");
             }
         });
+
 
 
 
